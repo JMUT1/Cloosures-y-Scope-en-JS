082fc67b6186c60734a20c31cdcbef631c3a6396
chemaT <chemaaa94@gmail.com>
2022-02-14 21:41:09 -0600

    global scope

---
+++ global.js
@@ -0,0 +1,13 @@
+var hello = "Hello";
+let world = "Hello Word";
+const helloWorld = "Hello World!";
+
+const anotherFunction = ()=>{
+    console.log(hello);
+    console.log (world);
+    console.log (helloWorld);
+}
+
+anotherFunction();
+
+
